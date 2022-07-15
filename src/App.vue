9ce527fe21c1d492f1311189a150baaf0ab8cee3
Vegadrone <spenaantonino@gmail.com>
2022-07-15 16:12:43 +0200

    Import Header, Main and Footer in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
-    
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from'./components/HeaderComponent.vue';
+import Main from './components/MainComponent.vue';
+import Footer from './components/FooterComponent.vue';
 
 
 export default {
   name: 'App',
   components: {
-  
+    Header,
+    Main,
+    Footer
   }
 }
 </script>
