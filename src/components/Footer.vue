4804231eb428c4c8fe0c25d749cc0a7994cdbf66
Vegadrone <spenaantonino@gmail.com>
2022-07-15 18:47:13 +0200

    Basic layout complete

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,100 @@
 <template>
   <footer>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                
-            </div>
+      <div class="row">
+        <div class="col-3 d-flex">
+          <h4>Dc comics</h4>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+          </ul>
+          <h4>Dc comics</h4>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+          </ul>
+          <h4>Dc comics</h4>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+          </ul>
+          <h4>Dc comics</h4>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+          </ul>
+          <h4>Dc comics</h4>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+          </ul>
         </div>
+      </div>
     </div>
     <Socials />
   </footer>
 </template>
 
 <script>
-import Socials from './Socials.vue'
+import Socials from "./Socials.vue";
 
 export default {
-    name:'Footer',
-    components:{
-        Socials,
-    }
-
-}
+  name: "Footer",
+  components: {
+    Socials,
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+    footer{
+        background-image: url(../assets/img/dc-logo-bg.png), url(../assets/img/footer-bg.jpg);
+        background-position: right bottom, left top;
+        background-repeat: no-repeat, repeat;
+    }
+
+    ul{
+        list-style: none;
+    }
 
 </style>
--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -2,10 +2,10 @@
   <nav>
     <div class="container">
       <div class="row">
-        <div class="col-12 d-flex">
-          <a href="">Sign up now!</a>
-          <h4>Follow us</h4>
+        <div class="col-12 d-flex justify-content-between">
+          <a href="#">Sign up now!</a>
           <ul>
+            <li><h4>Follow us</h4></li>
             <li><img src="../assets/img/footer-facebook.png" alt="facebook-logo"></li>
             <li><img src="../assets/img/footer-twitter.png" alt="twitter-logo"></li>
             <li><img src="../assets/img/footer-youtube.png" alt="youtube-logo"></li>
@@ -40,5 +40,22 @@ export default {};
     img{
         height: 3rem;
     }
+
+    a{
+        border: 2px solid $brandColor;
+        vertical-align: middle;
+        text-align: center;
+        padding: 1rem;
+    }
+
+    a,
+    h4{
+        text-decoration: none;
+        font-size: 1.8rem;
+        text-transform: uppercase;
+        color: white;
+    }
+
+    
 
 </style>
