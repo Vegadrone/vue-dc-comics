a5ca3ec4019ae5ec7fabb86d235787779d53fbb0
Vegadrone <spenaantonino@gmail.com>
2022-07-15 18:04:23 +0200

    Add Footer and Socials component

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+    <Socials />
+  </footer>
+</template>
+
+<script>
+import Socials from './Socials.vue'
+
+export default {
+    name:'Footer',
+    components:{
+        Socials,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Socials.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex">
+          <a href="">Sign up now!</a>
+          <h4>Follow us</h4>
+          <ul>
+            <li><img src="../assets/img/footer-facebook.png" alt="facebook-logo"></li>
+            <li><img src="../assets/img/footer-twitter.png" alt="twitter-logo"></li>
+            <li><img src="../assets/img/footer-youtube.png" alt="youtube-logo"></li>
+            <li><img src="../assets/img/footer-pinterest.png" alt="pinterest-logo"></li>
+            <li><img src="../assets/img/footer-periscope.png" alt="periscope-logo"></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+    @import "../styles/variables.scss";
+
+    nav{
+        background-color: $footerBgColor;
+    }
+
+    ul{
+        list-style: none;
+    }
+
+    li{
+        display: inline-block;
+    }
+    
+    img{
+        height: 3rem;
+    }
+
+</style>
