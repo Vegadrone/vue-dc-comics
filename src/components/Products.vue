184d28e15d91a4de5073ebf476cb0d7036ba1d40
Vegadrone <spenaantonino@gmail.com>
2022-07-18 19:36:40 +0200

    Product component complete

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="wrapper">
+  <div class="background">
     <div class="container">
-      <div class="row">
-        <div class="col-12" v-for="(img, index) in imgList" :key="index">
-            <img src="require(`../assets/img/buy-${img.imgPath}`)" alt="img.alt">
-
+      <div class="row d-flex">
+        <div class="col d-flex align-items-center" v-for="(img, index) in imgList" :key="index">
+            <img :src="require(`../assets/img/buy-${img.imgPath}`)" alt="img.alt">
+            <h6 class="text-light text-uppercase">{{img.text}}</h6>
         </div>
       </div>
     </div>
@@ -18,22 +18,27 @@ export default {
       imgList: [
         {
           imgPath: "comics-digital-comics.png",
+          text:"Digital Comics",
           alt: "digital-comics.png",
         },
         {
           imgPath: "comics-merchandise.png",
+          text:"Dc merchandise",
           alt: "merchandise.png",
         },
         {
           imgPath: "comics-shop-locator.png",
+          text:"Subscription",
           alt: "shop-locator.png",
         },
         {
           imgPath: "comics-subscriptions.png",
+          text:"Comic shop locator",
           alt: "subscriptions.png",
         },
         {
           imgPath: "dc-power-visa.svg",
+          text:"Dc power visa",
           alt: "dc-power-visa.svg",
         },
       ],
@@ -45,8 +50,14 @@ export default {
 <style lang="scss" scoped>
 @import "../styles/variables.scss";
 
-.wrapper {
+.background {
   background-color: $brandColor;
-  height: 15rem;
+  padding: 3rem;
+}
+
+img{
+    height: 4rem;
+    width: 3.3rem;
+    margin-right: 0.8rem;
 }
 </style>
